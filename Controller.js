6258777aae70605718df375029a2b5c371a3ccc4
Alex <alexander.simanchev@gmail.com>
2022-07-08 14:37:50 +0300

    almost there

--- Controller.js
+++ Controller.js
@@ -2,6 +2,7 @@ class Controller {
   constructor(model, view) {
     this.model = model
     this.view = view
+    this.quest = []
   }
 
   run() {
@@ -18,12 +19,14 @@ class Controller {
 
   getArrQuest(topicNumber) {
     this.model.readContent(`./topics/${topicNumber}.txt`)
-    .then(arr => this.printQuestController(arr))
+    .then(arr => {
+      this.quest = arr;
+      this.printQuestController(this.quest)})
   }
 
   printQuestController(arrQuest) {
     let answer = arrQuest[0][4]
-    let quest = arrQuest.splice(0,1)
+    const quest = arrQuest.splice(0,1)
     console.log('\x1b[32m%s\x1b[0m', `\n${quest[0][0]}`)
     console.log(`\n${quest[0].slice(1, 4).join('\n')}\n`)
 
@@ -31,14 +34,26 @@ class Controller {
       .then(number => this.getQuest(number))
   }
 
-  getQuest(QuestNumber) {
-    let quest = arrQuest.splice(0,1)
+  getQuest() {
+
+    if (this.quest.length === 0) return console.log('\nУСЕ!\n')
+    
+    let answer = this.quest[0][4]
+    const quest = this.quest.splice(0,1)
     console.log('\x1b[32m%s\x1b[0m', `\n${quest[0][0]}`)
-    console.log(`\n${quest[0].slice(1, 4).join('\n')}\n`)
+    console.log(`\n${quest[0].slice(1, 4).join('\n')}\n`) 
 
     this.view.getNumber()
-      .then(number => this.getQuest(number))
-      
+      .then(number => {
+        if (number !== answer) {
+          console.log('не вер но')
+        } else {
+          console.log('верно')
+        }
+        console.log('number', number)
+        return this.getQuest(number)
+      })
+
   }
 
 
