7a71b4bfef4951de07fc38bae7c9909112511a1e
serejqka <serejaseed@yandex.ru>
2022-07-08 14:47:26 +0300

    All VIEW DONE

--- View.js
+++ View.js
@@ -1,5 +1,4 @@
 const readline = require('readline');
-// const { stdin: input, stdout: output } = require('process');
 
 class View {
   constructor() {}
@@ -23,27 +22,6 @@ class View {
       })
     })
   }
-
-  showQuest(arrQuest) {
-    // console.log(arrQuest[0])
-    // console.log(arrQuest.slice(1).join('\n') + '\n')
-  }
-
 }
 
 module.exports = View
-
-const arr = ['tema1', 'tema2', 'tema3']
-// // const arrQ = ['1. Верно или нет? Еноты впадают в спячку.' , 'a. YES', 'b. NO', "c. MAYBE"]
-const obj = new View()
-obj.showTopic(arr)
-obj.getNumber()
-
-// const answer = rl.question('What do you think of Node.js? ');
-// console.log(`Thank you for your valuable feedback: ${answer}`);
-
-
-
-
-
-// '\x1b[32m%s\x1b[0m',
